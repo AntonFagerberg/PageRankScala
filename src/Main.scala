f76a5045bdbd718092b6b9fffa376043e4172cf5
Anton Fagerberg <anton@antonfagerberg.com>
2013-10-04 11:37:10 +0200

    Working.

--- src/Main.scala
+++ src/Main.scala
@@ -77,52 +77,88 @@ object Main {
     }
   }
 
-  def buildSpecialH(H: ArrayBuffer[ArrayBuffer[Double]]): ArrayBuffer[Double] = {
-    H.map(_.sum)
+  def printMap[K, V](map: mutable.HashMap[(K, K), V]) {
+    map.map { case ((row, column), value) =>
+      println(s"[$row, $column] = ${value.toString.toFloat.formatted("%.2f")}")
+    }
+    println()
+  }
+
+  def buildSparseFromFile(fileName: String): (Int, mutable.HashMap[(Int, Int), Double], mutable.HashMap[Int, Double]) = {
+    val scanner = new Scanner(io.Source.fromFile(fileName).bufferedReader())
+    val size = scanner.nextInt
+
+    val A = new mutable.HashMap[(Int, Int), Int]
+    val rowCount = new mutable.HashMap[Int, Int]
+
+    while (scanner.hasNextInt) {
+      val (row, column) = scanner.nextInt -> scanner.nextInt
+      rowCount(row) = rowCount.get(row).getOrElse(0) + 1
+      A(row -> column) = A.get(row -> column).getOrElse(0) + 1
+    }
+
+    val H = A.map { case ((row, column), value) =>
+      (row -> column) -> value.toDouble / rowCount(row)
+    }
+
+
+    val D = new mutable.HashMap[Int, Double]
+
+    (0 until size).foreach { row =>
+      if (!rowCount.contains(row)) {
+        D(row) = 1d / size
+      }
+    }
+
+    (size, H, D)
   }
 
   def main(args: Array[String]): Unit = {
-    val A = buildAFromFile("data/three.txt")
-    val size = A.length
-    val H = buildHFromA(A)
-    val D = buildDFromA(A)
-    val sparseH = buildSparseMatrixFromH(H)
+//    val A = buildAFromFile("data/three.txt")
+//    val size = A.length
+//    val H = buildHFromA(A)
+//    val D = buildDFromA(A)
+//    val sparseH = buildSparseMatrixFromH(H)
+
+    val (size, h, d) = buildSparseFromFile("data/p2p-Gnutella08-mod.txt")
     val alpha = 0.85d
     val oneMinusAlphaDividedByN = (1d - alpha) / size
-    val specialH = buildSpecialH(H)
-
     var p = ArrayBuffer.fill(size)(1d / size)
 
-    (0 until 10).foreach { index =>
+    (0 until 1000).foreach { index =>
       // Part 1
       val alphaP = p.map(alpha *)
       val alphaPH = ArrayBuffer.fill(size)(0d)
-      sparseH.map { case (row, column, value) =>
+      h.foreach { case ((row, column), value) =>
         alphaPH(column) += alphaP(row) * value
       }
+//      sparseH.map { case (row, column, value) =>
+//        alphaPH(column) += alphaP(row) * value
+//      }
 
-
-
-
-//      val alphaPH = alphaP.zip(specialH).map{ case (a, b) => a*b }
 
 
       // Part 2
-      val sumD = alphaP.zip(D).map { case (a, b) => a * b}.sum
+//      val sumD = alphaP.zip(D).map { case (a, b) => a * b}.sum
+
+      val sumD =
+        d.map { case (row, value) =>
+          alphaP(row) * value
+        }.sum
 
       // Part 3
-      val sumP = p.sum
-      val oneMinusAlphaDividedByNP = sumP * oneMinusAlphaDividedByN
+      val oneMinusAlphaDividedByNP = p.sum * oneMinusAlphaDividedByN
 
       p = alphaPH.map(oneMinusAlphaDividedByNP + sumD +)
     }
 
-    printVector(p)
-    printMatrix(A)
-    printMatrix(H)
-    printSparseMatrix(sparseH)
-    printVector(D)
-    printVector(p)
-//    printVector(alphaP)
+//    printMatrix(A)
+//    printMatrix(H)
+//    printSparseMatrix(sparseH)
+//    printVector(D)
+//    printVector(p)
+    p.zipWithIndex.sorted.map { case (value, index) =>
+      println(s"$index: $value")
+    }
   }
 }
